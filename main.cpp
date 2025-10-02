d10ed3317897f64beaf67b47e24f32e991645b52
RussellZhou-hub <938311288@qq.com>
2025-10-03 05:49:30 +0800

    drawing line

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+﻿#include <cmath>
 #include "tgaimage.h"
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
@@ -5,6 +6,29 @@ constexpr TGAColor green   = {  0, 255,   0, 255};
 constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
+
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep) { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx) { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int ierror = 0;
+    for (int x = ax; x <= bx; x++) {
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+        ierror += 2 * std::abs(by - ay);
+        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
+        ierror -= 2 * (bx - ax) * (ierror > bx - ax);
+    }
+}
 
 int main(int argc, char** argv) {
     constexpr int width  = 64;
@@ -18,6 +42,11 @@ int main(int argc, char** argv) {
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
     framebuffer.set(cx, cy, white);
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
